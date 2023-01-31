e536ef1c8586bcbfbd2ce65463fda48ab6abaa74
austinwarren <austingw2020@gmail.com>
2023-01-31 03:54:21 -0800

    was able to create a simple web server that has proper error handling. Working with Docker/flask

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:20.04
-MAINTAINER John Doe "noemail@website.domain"
+MAINTAINER Austin Warren "awarren3@uoregon.edu"
 RUN apt-get update -y
 RUN apt-get install python3.8 python3-pip -y
 COPY ./web /app
--- web/app.py
+++ web/app.py
@@ -1,14 +1,55 @@
 """
-John Doe's Flask API.
+Austin Warren's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, request, send_from_directory, abort
+import configparser
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+
+@app.route("/<path:path>")
+def pathCheck(path):
+    #name = request.args.get("nameRequest")
+    if os.path.exists("pages" + '/' + path):
+            #with open(path)as f:
+                #content = f.read()
+        return send_from_directory("pages" + '/', path), 200
+
+    elif("~" in path or ".." in path):
+        abort(403)
+
+    else:
+        abort(404)
+
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+config = parse_config(["credentials.ini", "default.ini"])
+#message = config["DEFAULT"]["message"]
+
+#print(message)
+
+@app.errorhandler(403)
+def forbidden(e):
+    return send_from_directory("pages/", "403.html"), 403
+
+@app.errorhandler(404)
+def notFound(e):
+    return send_from_directory("pages/", "404.html"), 404
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=config["SERVER"]["DEBUG"], host ='0.0.0.0', port = config["SERVER"]["PORT"])
